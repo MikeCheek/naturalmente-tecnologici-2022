cdd97513e0f0ff7bfee2086b305e16f0701b35be
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-05-07 19:50:57 +0200

    Added parallax

--- src/atoms/logo/logo.tsx
+++ src/atoms/logo/logo.tsx
@@ -1,15 +1,47 @@
 import { StaticImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useRef } from 'react';
 import * as styles from './logo.module.scss';
 
 const Logo = () => {
+  const logoRef = useRef<HTMLDivElement>(null);
+
+  const parallax = (event: MouseEvent): void => {
+    const shift = logoRef.current;
+    if (shift) {
+      const rect: DOMRect = shift.getBoundingClientRect();
+      const middleY: number = rect.top + rect.height / 2;
+      const middleX: number = rect.left + rect.width / 2;
+      const x: number = -(middleX - event.pageX) / 50;
+      const y: number = (middleY - event.pageY) / 50;
+
+      shift.style.transform = ` perspective(300px) rotateX(${y}deg) rotateY(${x}deg) scale(1.05)`;
+    }
+  };
+
+  const addListener = (): void | null => document.addEventListener('mousemove', parallax);
+
+  const removeListener = (): void => {
+    document.removeEventListener('mousemove', parallax);
+    const shift = logoRef.current;
+    if (shift) shift.style.removeProperty('transform');
+  };
+
   return (
-    <StaticImage
-      src="../../images/logo.png"
-      alt="Naturalmente Tecnologici Logo"
-      placeholder="blurred"
-      className={styles.logo}
-    />
+    <div
+      ref={logoRef}
+      className={styles.logoWrap}
+      onMouseLeave={removeListener}
+      onMouseOver={addListener}
+      onMouseOut={removeListener}
+      onTouchStart={() => {}}
+    >
+      <StaticImage
+        src="../../images/logo.png"
+        alt="Naturalmente Tecnologici Logo"
+        placeholder="blurred"
+        className={styles.logo}
+      />
+    </div>
   );
 };
 
