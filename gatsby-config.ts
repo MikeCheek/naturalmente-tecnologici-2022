1d7edaba9c073d57ad6aaeff524927f1ae051bae
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-05-07 15:50:48 +0200

    trying to fix errors

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,4 @@
-import type { GatsbyConfig } from 'gatsby';
-
-const config: GatsbyConfig = {
+module.exports = {
   siteMetadata: {
     title: `Naturalmente Tecnologici`,
     siteUrl: `https://www.yourdomain.tld`,
@@ -11,9 +9,19 @@ const config: GatsbyConfig = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: 'src/images/icon.png',
+        name: 'Naturalmente Tecnologici',
+        short_name: 'NT',
+        lang: 'it',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
     'gatsby-plugin-sharp',
@@ -24,8 +32,7 @@ const config: GatsbyConfig = {
         name: 'images',
         path: './src/images/',
       },
+      __key: 'images',
     },
   ],
 };
-
-export default config;
