47bdcc308c303b65f6a5533baaa0bf0cad2fd5b2
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-06-14 17:09:54 +0200

    Now is perfect

--- src/atoms/seo/seo.tsx
+++ src/atoms/seo/seo.tsx
@@ -9,7 +9,7 @@ const SEO = ({ description = '', lang = 'en', meta = [], title }: SEOProps): JSX
   const metaDescription: string = description; //|| site.siteMetadata.description
   //const defaultTitle = site.siteMetadata?.title
   const defaultTitle: string = 'Syskrack';
-  const image = 'https://nt.syskrack.org/logo-light.jpg';
+  const image = 'https://nt.syskrack.org/logo-og.jpg';
 
   return (
     <Helmet
