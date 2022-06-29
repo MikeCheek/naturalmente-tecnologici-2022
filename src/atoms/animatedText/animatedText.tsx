3678f46707dfdebfe944c9c7c0933f47113df797
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-06-29 12:29:13 +0200

    Edited things

--- src/atoms/animatedText/animatedText.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react';
-import * as styles from './animatedText.module.scss';
-
-const AnimatedText = ({ children }: { children: ReactNode }) => {
-  return <span className={styles.coming}>{children}</span>;
-};
-
-export default AnimatedText;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import AnimatedText from '../../atoms/animatedText/animatedText';
-import Syskrack from '../../atoms/syskrack/syskrack';
 import * as styles from './footer.module.scss';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>
+      <span>
         Developed by{' '}
-        <AnimatedText>
-          <a href="https://bit.ly/mikecheekgit" title="Michele Pulvirenti" target="_blank" rel="noopener noreferrer">
-            Michele Pulvirenti
-          </a>
-        </AnimatedText>
-      </p>
-      <p>
+        <a href="https://bit.ly/mikecheekgit" title="Michele Pulvirenti" target="_blank" rel="noopener noreferrer">
+          Michele Pulvirenti
+        </a>
+      </span>
+      <span>
         Designed by{' '}
-        <AnimatedText>
-          <a
-            href="https://www.instagram.com/raffaellacip/"
-            title="Raffaella Cipolla"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Raffaella Cipolla
-          </a>
-        </AnimatedText>
-      </p>
+        <a
+          href="https://www.instagram.com/raffaellacip/"
+          title="Raffaella Cipolla"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Raffaella Cipolla
+        </a>
+      </span>
     </div>
   );
 };
